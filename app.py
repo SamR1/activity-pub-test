65be49128c6dd6b62383455c48e65055fd682cac
Sam <sam@samr1.net>
2019-11-11 20:28:59 +0100

    add a route to post a note in reply to a toot on mastodon

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+import base64
 import json
 import os
 from dataclasses import dataclass
+from datetime import datetime
 
+import requests
+from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
-from flask import Flask, Response, request
+from Crypto.Signature import pkcs1_15
+from flask import Flask, Response, jsonify, request
 
 app = Flask(__name__)
 
@@ -92,7 +97,7 @@ def webfinger():
         ],
     }
     return Response(
-        json.dumps(resp), status=200, content_type="application/jrd+json"
+        json.dumps(resp), status=200, content_type='application/jrd+json'
     )
 
 
@@ -107,8 +112,69 @@ def get_actor(user):
     return Response(
         json.dumps(resp),
         status=200,
-        content_type="application/jrd+json; charset=utf-8",
+        content_type='application/jrd+json; charset=utf-8',
     )
+
+
+@app.route("/note", methods=["POST"])
+def post_note():
+    data = request.get_json()
+    message = (
+        data.get("message") if data else "<p>Trying to post a message!</p>"
+    )
+    host = data.get("host") if data else "mastodon.social"
+    toot = data.get("toot") if data else "@Gargron/100254678717223630"
+    toot_url = f"https://{host}/{toot}"
+
+    now = datetime.utcnow()
+    formatted_now = now.strftime("%a, %d %b %Y %H:%M:%S GMT")
+    activity = {
+        "@context": "https://www.w3.org/ns/activitystreams",
+        "id": f"https://{actor.domain}/create-test-note",
+        "type": "Create",
+        "actor": actor.ap_id,
+        "object": {
+            "id": f"https://{actor.domain}/note-message",
+            "type": "Note",
+            "published": now.strftime("%Y-%m-%dT%H:%M:%SZ"),
+            "attributedTo": actor.ap_id,
+            "inReplyTo": toot_url,
+            "content": message,
+            "to": "https://www.w3.org/ns/activitystreams#Public",
+        },
+    }
+
+    signed_string = (
+        f"(request-target): post /inbox\nhost: {host}\ndate: {formatted_now}"
+    )
+    key = RSA.import_key(actor.private_key)
+    key_signer = pkcs1_15.new(key)
+    encoded_string = signed_string.encode("utf-8")
+    h = SHA256.new(encoded_string)
+    signature = base64.b64encode(key_signer.sign(h))
+    header = (
+        f'keyId="{actor.ap_id}",headers="(request-target) host date",'
+        f'signature="' + signature.decode() + '"'
+    )
+
+    resp = requests.post(
+        f"https://{host}/inbox",
+        data=json.dumps(activity),
+        headers={
+            "Host": host,
+            "Date": formatted_now,
+            "Signature": header,
+            "Content-Type": "application/ld+json",
+        },
+    )
+    if resp.status_code >= 400:
+        print(resp.text)
+        response = {"status": "error"}
+    else:
+        response = {"status": "created"}
+    code = resp.status_code
+
+    return jsonify(response), code
 
 
 if __name__ == "__main__":
